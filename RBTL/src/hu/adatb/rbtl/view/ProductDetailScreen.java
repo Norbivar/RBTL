0a4be6e4ec5efea31e674f3a05d3521092a3a35d
Attila Uhrin <h670660@stud.u-szeged.hu>
2017-05-03 17:58:09 +0200

    Product details

--- RBTL/src/hu/adatb/rbtl/view/ProductDetailScreen.java
+++ RBTL/src/hu/adatb/rbtl/view/ProductDetailScreen.java
@@ -15,8 +15,8 @@ public class ProductDetailScreen extends JPanel{
 	private BookshopGUI gui;
 	
 	private JPanel data_panel, buy_panel;
-	private JLabel isbn_label, title_label, oldalszam_label, kotes_label, meret_label, ar_label, kiado_label, kiadaseve_label;
-	private JLabel isbn, title, oldalszam, kotes, meret, ar, kiado, kiadaseve;
+	private JLabel isbn_label, title_label, oldalszam_label, kotes_label, meret_label, ar_label, kiado_label, kiadaseve_label, szerzo_label;
+	private JLabel isbn, title, oldalszam, kotes, meret, ar, kiado, kiadaseve, szerzo;
 	
 	public ProductDetailScreen(BookshopGUI gui, Product product){
 		this.gui = gui;
@@ -25,7 +25,7 @@ public class ProductDetailScreen extends JPanel{
 		data_panel.setLayout(new GridLayout(0, 1));
 		
 		if(product instanceof Book){
-			Book book = null;
+			Book book = gui.getController().getBookByID(product.getId());
 			
 			isbn_label = new JLabel(Labels.PRODUCT_ISBN);
 			title_label = new JLabel(Labels.PRODUCT_TITLE);
@@ -35,6 +35,23 @@ public class ProductDetailScreen extends JPanel{
 			ar_label = new JLabel(Labels.PRODUCT_AR);
 			kiado_label = new JLabel(Labels.PRODUCT_KIADO);
 			kiadaseve_label = new JLabel(Labels.PRODUCT_KIADASEVE);
+			szerzo_label = new JLabel(Labels.PRODUCT_SZERZO);
+			
+			isbn = new JLabel(book.getIsbn());
+			title = new JLabel(book.getTitle());
+			oldalszam = new JLabel(String.valueOf(book.getNumOfPages()));
+			kotes = new JLabel(book.getKotesNev());
+			meret = new JLabel(book.getSize());
+			ar = new JLabel(String.valueOf(book.getPrice()));
+			//TODO kiado
+			kiadaseve = new JLabel(String.valueOf(book.getPublishYear()));
+			//TODO szerzo
+			
+			data_panel.add(isbn_label);
+			data_panel.add(isbn);
+			
+			data_panel.add(title_label);
+			data_panel.add(title);
 
 		} else if (product instanceof Film){
 
@@ -43,6 +60,7 @@ public class ProductDetailScreen extends JPanel{
 		} else {	//ebook
 
 		}
+		add(data_panel);
 	}
 
 }
