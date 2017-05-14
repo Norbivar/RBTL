dfe32bd2e4388047f20b41b4518350d4938669c8
Varju Norbert <norbivar96@gmail.com>
2017-05-14 09:37:13 +0200

    Presumably fixed UserCartScreen.

--- RBTL/src/hu/adatb/rbtl/view/UserCartScreen.java
+++ RBTL/src/hu/adatb/rbtl/view/UserCartScreen.java
@@ -22,21 +22,21 @@ import hu.adatb.rbtl.model.beans.Product;
 public class UserCartScreen extends JPanel  implements ActionListener { // I seriously doubt this will work but hey...
 
 	private BookshopGUI gui;
-	private JTable table; 
 	JButton checkout;
 	//private JLabel a;
 	HashMap<Product, Integer> cart;
-	public UserCartScreen(BookshopGUI gui){
-		super();
-		this.cart = gui.getController().getUserCart(gui.getController().getLoggedinUser());
-		this.gui = gui;
-
-		table = new JTable();
-		DefaultTableModel dm = new DefaultTableModel();
-		dm.setColumnIdentifiers(new Object[] { Labels.CART_ID_AND_TITLE, Labels.CART_AMOUNT_OF_ITEM, Labels.CART_DELETE });
+	public void Refresh()
+	{
+		this.removeAll();
+		int rows = cart.keySet().size();
+		setLayout(new GridLayout(3, rows+1));
+		add(new JLabel(Labels.CART_ID_AND_TITLE));
+		add(new JLabel(Labels.CART_AMOUNT_OF_ITEM));
+		add(new JLabel(Labels.CART_DELETE));
+		
 		for(Product tmp : cart.keySet())
 		{
-				System.out.println("DEBUG: talaltam cuccot a cartodban!");
+				//System.out.println("DEBUG: talaltam cuccot a cartodban!");
 				JLabel a1 = new JLabel(tmp.getId() + " " + tmp.getTitle());
 				JTextField a2 = new JTextField(cart.get(tmp).intValue());
 				SpecialJButton tmp2 = new SpecialJButton(Labels.CART_DELETE, tmp);
@@ -61,34 +61,37 @@ public class UserCartScreen extends JPanel  implements ActionListener { // I ser
 					     gui.getController().ModifyProductInUserCart(gui.getController().getLoggedinUser(), tmp2.getP(), Integer.parseInt(a2.getText()));
 					  }
 					});
-				//TODO: Fuck Java and reimplement this in gridlayout
-				//dm.insertRow(dm.getRowCount(), v);
+				add(a1);
+				add(a2);
+				add(tmp2);
 		}
-		table.setModel(dm);
-		add(table);
+		
 		checkout = new JButton(Labels.CART_CHECKOUT_MENUITEM);
 		add(checkout);
 		checkout.addActionListener(this);
+	}
+	
+	public UserCartScreen(BookshopGUI gui){
+		super();
+		this.cart = gui.getController().getUserCart(gui.getController().getLoggedinUser());
+		this.gui = gui;
+
+		Refresh();
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		if(e.getSource() instanceof SpecialJButton)
 		{
-			int counter = -1;
 			Product asd = ((SpecialJButton) e.getSource()).getP();
-			for(Product tmp : cart.keySet())
+			if(cart.remove(asd) != null)
 			{
-				counter++;
-				if(tmp.getId() == asd.getId()) {
-					((DefaultTableModel)table.getModel()).removeRow(counter);
-					if(gui.getController().DeleteFromUserCart(gui.getController().getLoggedinUser(), asd))
-						break;
-					else
-						System.out.println("ERROR: Could not delete item from cart!");
-				}
+				if(gui.getController().DeleteFromUserCart(gui.getController().getLoggedinUser(), asd))
+					Refresh();
+				else System.out.println("DEBUG: No item to delete IN CART found!");
 			}
-			table.updateUI();
+			else System.out.println("DEBUG: No item to delete was found!");
+			this.revalidate();
 		}
 		else if(e.getSource() == checkout)
 		{
